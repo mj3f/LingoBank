a862cc5a90969434841865eca59310c3a554291c
Mike Pratt <mikep2419@gmail.com>
2020-07-21 16:19:20 +0100

    Added endpoints for getting user-created languages from the database, haven't decided yet how the phrases should be retrieved.

--- src/LingoBank.API/Controllers/LanguagesController.cs
+++ src/LingoBank.API/Controllers/LanguagesController.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Threading.Tasks;
 using LingoBank.Core;
 using LingoBank.Core.Commands;
 using LingoBank.Core.Dtos;
+using LingoBank.Core.Queries;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LingoBank.API.Controllers
@@ -25,8 +27,42 @@ namespace LingoBank.API.Controllers
         {
             try
             {
-                await _runtime.ExecuteCommandAsync(new CreateLanguageCommand {LanguageDto = language});
+                await _runtime.ExecuteCommandAsync(new CreateLanguageCommand {Language = language});
                 return Ok("Language created");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<LanguageDto>), 200)]
+        [ProducesResponseType(400)]
+        [Description("Returns a list of all languages created by the user.")]
+        public async Task<IActionResult> GetLanguagesAsync()
+        {
+            try
+            {
+                List<LanguageDto> languages = await _runtime.ExecuteQueryAsync(new GetLanguagesQuery());
+                return Ok(languages);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(LanguageDto), 200)]
+        [ProducesResponseType(400)]
+        [Description("Returns a language created by the user.")]
+        public async Task<IActionResult> GetLanguageByIdAsync(string id)
+        {
+            try
+            {
+                LanguageDto language = await _runtime.ExecuteQueryAsync(new GetLanguageByIdQuery { Id = id });
+                return Ok(language);
             }
             catch (Exception ex)
             {
