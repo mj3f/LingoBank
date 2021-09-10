5a73eff0d060ccbb62506a53e79192b4074147c8
Mike Pratt <mike@msjp.me>
2021-09-10 19:30:25 +0100

    Added endpoints for getting a refresh token, as well as the details for the current logged in user.

--- src/LingoBank.API/Controllers/AuthController.cs
+++ src/LingoBank.API/Controllers/AuthController.cs
@@ -1,9 +1,13 @@
 using System.ComponentModel;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using LingoBank.API.Authentication;
 using LingoBank.Core;
 using LingoBank.Core.Dtos;
 using LingoBank.Core.Queries;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
@@ -11,6 +15,7 @@ namespace LingoBank.API.Controllers
 {
     [ApiController]
     [Produces("application/json")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/v0/auth")]
     public class AuthController : ControllerBase
     {
@@ -24,7 +29,9 @@ namespace LingoBank.API.Controllers
         }
 
         [HttpPost("login")]
+        [AllowAnonymous]
         [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
         [Description("Returns a jwt token if the user exists.")]
         public async Task<IActionResult> Login([FromBody] UserWithPasswordDto userWithPassword)
         {
@@ -53,8 +60,49 @@ namespace LingoBank.API.Controllers
             return Ok(token);
         }
         
-        // TODO: Endpoint for getting current logged in user.
+
+        [HttpGet("current-user")]
+        [ProducesResponseType(typeof(UserDto), 200)]
+        [ProducesResponseType(401)] 
+        [ProducesResponseType(400)]
+        [Description("Returns the current logged in users details.")]
+        public async Task<IActionResult> GetCurrentUserAsync()
+        {
+            string? email = User.FindFirst(ClaimTypes.Email)?.Value;
+
+            if (email is null)
+            {
+                return BadRequest("No email in the jwt token found.");
+            }
+
+            var user = await _runtime.ExecuteQueryAsync(new GetUserByIdQuery { EmailAddress = email });
+
+            // Don't assume the user exists despite a jwt token being provided -
+            // what if the user is deleted and the token is still somehow valid at this point?
+            if (user is null)
+            {
+                return BadRequest("No user found in the database with the email address provided.");
+            }
+
+            return Ok(user);
+        }
         
-        // TODO: Endpoint for getting a refresh token.
+        
+        [HttpGet("refresh")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(401)]
+        [Description("Returns a refreshed jwt token (without the user needing to login again) if the user exists.")]
+        public async Task<IActionResult> GetRefreshTokenAsync()
+        {
+            string? email = User.FindFirst(ClaimTypes.Email)?.Value;
+            
+            if (email is not null)
+            {
+                string token = await _jwtTokenGenerator.BuildToken(email);
+                return Ok(token);
+            }
+
+            return BadRequest("Could not generate a refreshed JWT token.");
+        }
     }
 }
