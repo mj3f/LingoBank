e03d766a9648a86d9e154f809bd4a61f98bf2e9b
Mike Pratt <mike@msjp.me>
2021-09-10 18:57:52 +0100

    Added configuration for Serilog.

--- src/LingoBank.API/Program.cs
+++ src/LingoBank.API/Program.cs
@@ -1,5 +1,8 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace LingoBank.API
 {
@@ -7,11 +10,31 @@ namespace LingoBank.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // https://github.com/serilog/serilog-aspnetcore
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+            
+            try
+            {
+                Log.Information("Starting Web Host...");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Failed to setup Serilog!");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
