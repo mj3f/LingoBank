275f5dc6a569c85ce8967c238fcb165a6b24fcc4
Mike Pratt <mike@msjp.me>
2021-09-19 19:37:05 +0100

    Added two tests to test creating a user, one valid and one invalid.

--- lingo/test/LingoBank.API.UnitTests/Controllers/UsersControllerTests.cs
+++ lingo/test/LingoBank.API.UnitTests/Controllers/UsersControllerTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using LingoBank.API.Controllers;
 using LingoBank.Core;
+using LingoBank.Core.Commands;
 using LingoBank.Core.Dtos;
 using LingoBank.Core.Queries;
 using Microsoft.AspNetCore.Mvc;
@@ -48,6 +49,53 @@ namespace LingoBank.API.UnitTests.Controllers
             IActionResult result = await sut.GetById(id);
 
             Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldCreateUser()
+        {
+            // Arrange
+            var user = new UserWithPasswordDto
+            {
+                Id = Guid.NewGuid().ToString(),
+                EmailAddress = "test@example.com",
+                UserName = "Test",
+                Role = "User",
+                Password = "Test12345678"
+            };
+           
+            var runtime = new Mock<IRuntime>();
+            
+            // Don't need to mock this as a command does not return anything. (so we assume the input is valid for this test)
+            // runtime.Setup(x => x.ExecuteCommandAsync(It.IsAny<CreateUserCommand>()));
+
+            var sut = new UsersController(runtime.Object);
+            
+            // Act
+            IActionResult result = await sut.Create(user);
+            
+            // Assert (assume the user was valid and it was created.
+            string okRes = (result as OkObjectResult)?.Value as string ?? string.Empty;
+            
+            Assert.Equal("User created.", okRes);
+        }
+
+        [Fact]
+        public async Task CreateAsync_WithNullInput_ShouldNotCreateUser()
+        {
+            // Arrange
+            UserWithPasswordDto user = null;
+           
+            var runtime = new Mock<IRuntime>();
+            runtime.Setup(x => x.ExecuteCommandAsync(It.IsAny<CreateUserCommand>())).ThrowsAsync(new Exception());
+            
+            var sut = new UsersController(runtime.Object);
+            
+            // Act
+            IActionResult result = await sut.Create(user);
+            
+            // Assert (assume the user was valid and it was created.
+            Assert.IsType<BadRequestObjectResult>(result);
         }
     }
 }
