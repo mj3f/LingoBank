50cd6d099539a5d16902f2ae7c745c33a19273d7
Mike Pratt <mike@msjp.me>
2022-04-13 23:26:26 +0100

    Fixed ESLint warnings in button component

--- lingo/src/LingoBank.WebApp/src/app/shared/components/button/button.component.ts
+++ lingo/src/LingoBank.WebApp/src/app/shared/components/button/button.component.ts
@@ -1,14 +1,14 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
-  templateUrl: './button.component.html',
+	selector: 'app-button',
+	templateUrl: './button.component.html',
 })
 export class ButtonComponent { // TODO: Start using this.
 	@Output()
 	public onClick: EventEmitter<any> = new EventEmitter<any>();
 
-	handleClick(event) {
+	handleClick(event): void {
 		this.onClick.emit(event);
 	}
 
