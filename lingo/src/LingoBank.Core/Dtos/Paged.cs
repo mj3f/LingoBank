17cc934d2a2a66199fd2e08922c573fd9f36ff16
Mike Pratt <mike@msjp.me>
2022-06-14 16:30:29 +0100

    Changed how the next and prev page properties in Paged are calculated.

--- lingo/src/LingoBank.Core/Dtos/Paged.cs
+++ lingo/src/LingoBank.Core/Dtos/Paged.cs
@@ -1,17 +1,38 @@
+using System;
 using System.Collections.Generic;
+using LingoBank.Core.Constants;
 
 namespace LingoBank.Core.Dtos;
 
+/// <summary>
+/// Paginated sub-list of data.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class Paged<T> where T : class
 {
+    /// <summary>
+    /// The data fetched from the database.
+    /// </summary>
     public IList<T>? Data { get; }
     
+    /// <summary>
+    /// Total number of items/data in the database.
+    /// </summary>
     public int Total { get; }
 
+    /// <summary>
+    /// Current page number.
+    /// </summary>
     public int PageNumber { get; }
     
+    /// <summary>
+    /// The next page of data.
+    /// </summary>
     public int NextPage { get; }
     
+    /// <summary>
+    /// The previous page of data.
+    /// </summary>
     public int PrevPage { get; }
 
     public Paged(IList<T>? data, int total, int pageNumber)
@@ -19,7 +40,9 @@ public class Paged<T> where T : class
         Data = data;
         Total = total;
         PageNumber = pageNumber;
-        NextPage = pageNumber + 1 < total ? pageNumber + 1 : total;
-        PrevPage = pageNumber - 1 > 0 ? pageNumber - 1 : 1;
+        
+        double result = total / (double) CoreConstants.PagedNumberOfItemsPerPage;
+        NextPage = (int) Math.Ceiling(result);
+        PrevPage = NextPage == 1 ? 1 : NextPage - 1;
     }
 }
